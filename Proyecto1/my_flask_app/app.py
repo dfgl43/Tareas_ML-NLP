cf3b2f319a6f6308137bcf1d436d77eb01a6b494
Diego Fernando González Larrote <70554396+dfgl43@users.noreply.github.com>
2024-04-29 03:05:11 -0500

    Add files via upload

--- Proyecto1/my_flask_app/app.py
+++ Proyecto1/my_flask_app/app.py
@@ -54,4 +54,4 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
